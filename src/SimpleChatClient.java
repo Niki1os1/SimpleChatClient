09f58fbe134a738fc4a0681b005dec730b2f36ef
Niki1os <82583494+Niki1os1@users.noreply.github.com>
2021-10-27 18:59:04 +0300

    Update SimpleChatClient.java

--- src/SimpleChatClient.java
+++ src/SimpleChatClient.java
@@ -26,7 +26,7 @@ public class SimpleChatClient {
 
         String action;
         do {
-            System.out.print("Выберите действие: ");
+            System.out.print("Choose an action: ");
             action = scanner.nextLine();
 
             if ("help".equals(action)) {
@@ -46,7 +46,7 @@ public class SimpleChatClient {
             } else if ("9".equals(action)) {
                 readMessages();
             } else if(!":q".equals(action)){
-                System.out.println("Неизвестная команда, введите \"help\".");
+                System.out.println("Unknown command, enter \"help\".");
             }
         } while (!":q".equals(action));
 
@@ -57,7 +57,7 @@ public class SimpleChatClient {
         writer.println("1");
         writer.flush();
 
-        System.out.print("Введите имя: ");
+        System.out.print("Enter your name: ");
         name = scanner.nextLine();
         writer.println(name);
         writer.flush();
@@ -67,7 +67,7 @@ public class SimpleChatClient {
         writer.println("2");
         writer.flush();
         String line;
-        System.out.println("Список пользователей:");
+        System.out.println("A list of users:");
         do {
             line = reader.readLine();
             if (!"end".equals(line)) {
@@ -78,7 +78,7 @@ public class SimpleChatClient {
 
     private void sendAllMsg() throws IOException {
         writer.println("3");
-        System.out.print("Введите сообщение: ");
+        System.out.print("Enter your message: ");
         String msg = scanner.nextLine();
         writer.println(msg);
         writer.flush();
@@ -86,16 +86,16 @@ public class SimpleChatClient {
 
     private void sendPrivateMsg() throws IOException {
         writer.println("4");
-        System.out.print("Введите имя: ");
+        System.out.print("Enter your name: ");
         String name = scanner.nextLine();
         writer.println(name);
         writer.flush();
         String answer = reader.readLine();
         if ("404".equals(answer)) {
-            System.out.println("Такого пользователя не существует.");
+            System.out.println("No such user exists.");
             return;
         } else if ("200".equals(answer)) {
-            System.out.print("Введите сообщение: ");
+            System.out.print("Enter your message: ");
             String msg = scanner.nextLine();
             writer.println(msg);
             writer.flush();
@@ -106,7 +106,7 @@ public class SimpleChatClient {
         writer.println("9");
         writer.flush();
         String msg;
-        System.out.println("Входящие сообщения: ");
+        System.out.println("Incoming messages: ");
         do {
             msg = reader.readLine();
             if (!":end".equals(msg)) {
@@ -116,44 +116,44 @@ public class SimpleChatClient {
     }
 
     private void getChatTopic() throws IOException {
-        System.out.print("Введите имя пользователя, для которого хотите узнать тему чата: ");
+        System.out.print("Enter the name of the user for whom you want to know the chat subject: ");
         String name = scanner.nextLine();
         writer.println("6");
         writer.println(name);
         writer.flush();
         String answer = reader.readLine();
         if ("200".equals(answer)) {
-            System.out.println("Тема чата: " + reader.readLine());
+            System.out.println("Chat subject: " + reader.readLine());
         } else {
-            System.out.println("Тема чата не задана или пользователя не существует.");
+            System.out.println("The chat subject has not been set or the user does not exist.");
         }
     }
 
     private void setCharTopic() throws IOException {
-        System.out.print("Введите имя пользователя, для которого хотите задать тему чата:");
+        System.out.print("Enter the name of the user for whom you want to set a chat subject:");
         String name = scanner.nextLine();
         writer.println("5");
         writer.println(name);
         writer.flush();
         String answer = reader.readLine();
         if ("200".equals(answer)) {
-            System.out.print("Введите тему чата: ");
+            System.out.print("Enter the chat subject: ");
             String chatTopic = scanner.nextLine();
             writer.println(chatTopic);
             writer.flush();
         } else {
-            System.out.println("Такого пользователя нету");
+            System.out.println("There is no such user.");
         }
     }
 
     private void help() {
-        System.out.println("1 - ввести имя");
-        System.out.println("2 - список пользователей");
-        System.out.println("3 - сообщение всем");
-        System.out.println("4 - приватное сообщение");
-        System.out.println("5 - установить тему чата");
-        System.out.println("6 - получить тему чата");
-        System.out.println("9 - прочитать входящие сообщения");
-        System.out.println(":q - выход");
+        System.out.println("1 - enter name");
+        System.out.println("2 - a list of users");
+        System.out.println("3 - message to all");
+        System.out.println("4 - private message");
+        System.out.println("5 - set chat subject");
+        System.out.println("6 - get chat topic");
+        System.out.println("9 - read incoming messages");
+        System.out.println(":q - exit");
     }
 }
